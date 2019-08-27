384d18d4b82402390cd2abce2b854c07482bdaa7
Matt Pitner <mattpitner09@gmail.com>
2019-08-27 20:55:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array)
+    array.select do |word1|
+      word1.split(" ")
+      
+    
+  end 
+  
+  
+end 
